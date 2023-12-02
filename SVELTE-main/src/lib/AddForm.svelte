033ff1d33dae96af1c8c6848f6fdab90227d2fee
NickBrb <125358659+NickBrb@users.noreply.github.com>
2023-12-02 11:42:49 +0100

    Add files via upload

---
+++ SVELTE-main/src/lib/AddForm.svelte
@@ -0,0 +1,21 @@
+<script>
+    import {todos} from "./store"
+
+    let nuovoTodo
+
+    function AddTodo() {
+        todos.update((old) =>{
+            old.push({
+                title : nuovoTodo, 
+                done : false 
+            }) 
+            return old
+        })
+    }
+
+</script>
+
+<input type ="text" bind:value={nuovoTodo} class="input input-bordered w-full max-w-xs"/>
+<br><br>
+<button on:click={AddTodo} class="btn btn-outline btn-info"> Aggiungi </button>
+
