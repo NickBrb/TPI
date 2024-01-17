5cdd0944fb4c148b8848a58a55d809b20b7e7c53
NickBrb <125358659+NickBrb@users.noreply.github.com>
2024-01-17 15:26:56 +0000

    grrpow

--- SVELTE-main/src/App.svelte
+++ SVELTE-main/src/App.svelte
@@ -8,7 +8,7 @@
 </script>
 
 <main>
-  <h1 class="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-blue-600 dark:text-blue-500">TO DO LIST </h1>  
+  <h1 class="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-blue-900 dark:text-blue-600">TO DO LIST </h1>  
   <TodoCounter />
   <AddForm />
   <ListaTodo todoList ={$todos} />
--- SVELTE-main/src/lib/AddForm.svelte
+++ SVELTE-main/src/lib/AddForm.svelte
@@ -15,7 +15,7 @@
 
 </script>
 
-<input type ="text" bind:value={nuovoTodo} class="input input-bordered w-full max-w-xs"/>
+<input  bind:value={nuovoTodo} type="text" placeholder="Scrivi qui..." class="input input-bordered w-full max-w-xs mt-2"/>
 <br>
 <button on:click={AddTodo}  class="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 mt-4">Add </button>
 
