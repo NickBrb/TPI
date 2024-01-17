1dd3aba519e2e69ecb9ff4135a024b0384c222bf
NickBrb <125358659+NickBrb@users.noreply.github.com>
2024-01-17 09:28:08 +0000

    lore

--- SVELTE-main/src/lib/AddForm.svelte
+++ SVELTE-main/src/lib/AddForm.svelte
@@ -16,6 +16,6 @@
 </script>
 
 <input type ="text" bind:value={nuovoTodo} class="input input-bordered w-full max-w-xs"/>
-<br><br>
-<button on:click={AddTodo}  class="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow"> Add... </button>
+<br>
+<button on:click={AddTodo}  class="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 mt-4">Add </button>
 
